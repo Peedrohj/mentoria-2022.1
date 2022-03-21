db896d1220cc4d1b8dc1074d83239186f35f11c2
Pedro Farias <phjfarias@hotmail.com>
2022-03-20 23:43:25 -0300

    feat: add basic array operations

---
+++ 0 - Estrutura de dados/array.py
@@ -0,0 +1,38 @@
+from typing import List
+
+# Exemplo básico de lista
+lista_de_numeros: List[int] = [1, 2, 3, 3, 4, 5]
+lista_de_strings: List[str] = ["a", "b", "c", "d", "e"]
+lista_de_objetos: List[dict] = [{
+    "nome": "Andre",
+    "idade": 12
+}, {
+    "nome": "Luciana",
+    "idade": 51
+}]
+lista_de_lista: List[List] = [[1, 2, 3, 3, 4, 5],
+                  ["a", "b", "c", "d", "e"]]
+
+# Adicinoar um elemento ao final da lista
+lista_de_numeros.append(6)
+[1, 2, 3, 3, 4, 5, 6]
+
+# Adicinoar um elemento a uma posição da lista
+lista_de_numeros.insert(0, 6)
+[6, 1, 2, 3, 3, 4, 5]
+
+# Remover um item da lista
+# OBS: Essa função vai remover apenas a primera ocorrência
+lista_de_numeros.remove(1)
+[2, 3, 3, 4, 5]
+
+# Remover o último elemento da lista
+lista_de_numeros.pop()
+[1, 2, 3, 3, 4]
+
+# Remover um elemento pelo index
+lista_de_numeros.pop(1)
+[1, 3, 3, 4, 5]
+
+# 
+next(x for x in lista_de_objetos)
---
+++ 0 - Estrutura de dados/dict.py
@@ -0,0 +1,11 @@
+from typing import Dict, List
+
+# Exemplo básico de dicionário
+dicionario_de_pessoas: Dict = {
+    "Andre": {
+        "idade": 12
+    },
+    "Luciana": {
+        "idade": 21
+    }
+}
