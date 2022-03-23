78fbd48452b56feda9b2abae9e1184415e050a93
Pedro Farias <phjfg@cesar.org.br>
2022-03-23 00:39:54 -0300

    feat: add tuple example

---
+++ 0 - Introduacao/tuple/tuple.py
@@ -0,0 +1,14 @@
+from typing import Tuple, List
+
+# Exemplo básico de um tupla
+tupla_de_numeros: Tuple[int] = (1, 2, 3, 4, 5)
+tupla_de_strings: Tuple[str] = ("a", "b", "c", "d", "e")
+tupla_de_objetos: Tuple[dict] = ({
+    "nome": "Andre",
+    "idade": 12
+}, {
+    "nome": "Luciana",
+    "idade": 51
+})
+tupla_de_lista: Tuple[List] = ([1, 2, 3, 3, 4, 5],
+                               ["a", "b", "c", "d", "e"])
